308c46a72ebb712bf36c36b797740bc597e57d09
Matt Condon <MattCMultimedia@gmail.com>
2014-06-27 19:47:51 -0400

    add peewee models

--- models.py
+++ models.py
@@ -4,19 +4,18 @@ import datetime
 
 db = MySQLDatabase(db_name, user=db_user, passwd=db_pass, host=db_host)
 
-class OCModel(Model):
+class YoModel(Model):
     class Meta:
         database = db
 
+class Yoser(YoModel):
+    YoserID = PrimaryKeyField(primary_key=True, auto_increment=True)
+    YoserName = CharField(100)
+    Endpoint = TextField()
+    TSAdded = DateTimeField(default=datetime.datetime.now)
 
-
-
-class Department(OCModel):
-    DeptID = PrimaryKeyField(primary_key=True, auto_increment=True)
-    Dept = CharField(100)
-
-    def jsonify(self):
-        return {
-            'DeptID': self.DeptID,
-            'Dept': self.Dept
-        }
+class Yo(YoModel):
+    YoID = PrimaryKeyField(primary_key=True, auto_increment=True)
+    YoFrom = ForeignKeyField(Yoser, related_name="yos_sent")
+    YoTo = ForeignKeyField(Yoser, related_name="yos")
+    TSAdded = DateTimeField(default=datetime.datetime.now)
